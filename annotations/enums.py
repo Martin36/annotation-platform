2422fda29acc4a499dd8f8a3bdb3095807c833de
Martin <martin.funkquist@gmail.com>
2022-03-06 09:27:01 +0100

    Added empty category to the categories dropdown

--- annotations/enums.py
+++ annotations/enums.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 class Category(models.TextChoices):
+  NONE = "", _("None")
   OPINION = "Opinion", _("Opinion")
   PREDICTION = "Prediction", _("Prediction")
   IMPOSSIBLE = "Impossible", _("Impossible")
